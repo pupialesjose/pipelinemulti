d4777b6206ede084e1fe74a8337dc397c5562642
pupialesjose <pupialesj@gmail.com>
2026-01-03 12:50:20 -0500

    Update main.tf

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -28,9 +28,17 @@ resource "aws_internet_gateway" "igw" {
 # Route Table
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.calculadora_vpc.id
-  route { cidr_block = "0.0.0.0/0", gateway_id = aws_internet_gateway.igw.id }
-  tags = { Name = "calculadora-public-rt" }
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "calculadora-public-rt"
+  }
 }
+
 
 resource "aws_route_table_association" "public_assoc" {
   subnet_id      = aws_subnet.public_subnet.id
