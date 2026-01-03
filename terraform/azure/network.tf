6ad150b874807f19d8bfda09cc7bb880036e35d5
pupialesjose <pupialesj@gmail.com>
2026-01-03 17:13:03 -0500

    Update network.tf

--- terraform/azure/network.tf
+++ terraform/azure/network.tf
@@ -28,6 +28,19 @@ resource "azurerm_network_security_group" "nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+
+# Nueva regla para la aplicación en el puerto 8080
+  security_rule {
+    name                       = "APP_HTTP"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8080"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 resource "azurerm_public_ip" "pip" {
